7037dfe05c88980b4323691ff35ab4ee6eaf47d5
Thomas Cemeli <thomasdevpro38@gmail.com>
2025-12-18 17:32:15 +0100

    feat: init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,15 @@
+import { Layout } from './modules/app/components/Layout'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <Layout>
+        <h1>Hello world !</h1>
+      </Layout>
+    </>
+  )
+}
+
+export default App
---
+++ frontend/src/modules/app/components/Layout.tsx
@@ -0,0 +1,18 @@
+import { CssBaseline, Toolbar, Typography, Container } from "@mui/material";
+
+export const Layout: React.FC <{children: React.ReactNode}> = ({children}) => {
+
+    return (
+        <>
+            <CssBaseline />
+            <Toolbar sx={({borderBottom: 1, borderBottomColor: 'divider'})}>
+                <Typography variant="h6" noWrap component="div" sx={({flexGrow: 1})}>
+                    EventHub
+                </Typography>
+            </Toolbar>
+            <Container maxWidth="lg" sx={({ mt: 4})}>
+                {children}
+            </Container>
+        </>
+    )
+}
